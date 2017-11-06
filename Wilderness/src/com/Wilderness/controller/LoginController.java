98fcd5fbf58c2ae31fae3dab809651b6860f2e5f
Matthew McDaniel <Matthew McDaniel@Matthew-PC.attlocal.net>
2017-11-06 17:03:59 -0500

    Allows users to login, however does not allow them to register yet. Logging in just compares the user name and password to the database. Also created two model classes for User and Employee.

--- Wilderness/src/com/Wilderness/controller/LoginController.java
+++ Wilderness/src/com/Wilderness/controller/LoginController.java
@@ -1,5 +1,15 @@
 package com.Wilderness.controller;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.Wilderness.model.User;
+
+import application.WildernessDBConfig;
+import javafx.beans.property.ObjectPropertyBase;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -7,18 +17,45 @@ import javafx.scene.control.TextField;
 
 public class LoginController {
 	// All objects used in the Login side of the split pane.
-	@FXML TextField txtUserNameLogin;
-	@FXML TextField txtPasswordLogin;
-	@FXML Label lblUserNameFeedbackLogin; // Displays any user name errors when logging in.
-	@FXML Label lblPasswordFeedbackLogin; // Displays any password errors when logging in.
-	@FXML Button btnLogin;
+	@FXML private TextField txtUserNameLogin;
+	@FXML private TextField txtPasswordLogin;
+	@FXML private Label lblUserNameFeedbackLogin; // Displays any user name errors when logging in.
+	@FXML private Label lblPasswordFeedbackLogin; // Displays any password errors when logging in.
+	@FXML private Button btnLogin;
 	// All objects used in the Register side of the split pane.
-	@FXML Button btnRegisterNewUser;
-	@FXML TextField txtAdminIDRegister;
-	@FXML TextField txtUserNameRegister;
-	@FXML TextField txtPasswordRegister;
-	@FXML Label lblAdminIDFeedbackRegister; // Displays any Admin ID errors when registering.
-	@FXML Label lblUserNameFeedbackRegister; // Displays any user name errors when registering.
-	@FXML Label lblPasswordFeedbackRegister; //Displays any password errors when registering.
+	@FXML private Button btnRegisterNewUser;
+	@FXML private TextField txtAdminIDRegister;
+	@FXML private TextField txtUserNameRegister;
+	@FXML private TextField txtPasswordRegister;
+	@FXML private Label lblAdminIDFeedbackRegister; // Displays any Admin ID errors when registering.
+	@FXML private Label lblUserNameFeedbackRegister; // Displays any user name errors when registering.
+	@FXML private Label lblPasswordFeedbackRegister; //Displays any password errors when registering.
 	
+	//public final void onActionProperty() throws SQLException{
+	//	PreparedStatement ps = WildernessDBConfig.getConnection().prepareStatement("SELECT * FROM user WHERE email = ? and password = ?");
+	//}
+	public void initialize() {
+		
+	}
+    public final void onLoginButtonPushed(ActionEvent event) {
+    	User user = new User(txtUserNameLogin.getText(),txtPasswordLogin.getText());
+    	try{
+		if(user.validLogin(txtUserNameLogin.getText(), txtPasswordLogin.getText())) {
+			//Will add method that loads main page later.
+			lblUserNameFeedbackLogin.setText("Login Successful");
+		}
+			else {
+				lblUserNameFeedbackLogin.setText("Incorrect user name or password");
+			}
+			
+		}
+    	catch(Exception a) {
+    		lblUserNameFeedbackLogin.setText("Error");
+    	}
+    		
+    		
+    	
+		
+    }
+
 }
